5239d9682aa976b374afd5fcca69e177e98f4454
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-18 17:51:52 +1100

    Created new home page component that loads the content for the page depending on what you are logged in as

---
+++ teach-team-app/src/components/Home/Home.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import "./Home.css";
+
+interface EducatorProps
+{
+    isLoggedIn: Boolean, //Boolean to track if the user is logged in
+    accountType: String; //String that determines if it loads Lecturer or Tutor version of the page
+}
+
+interface SubjectProps
+{
+    subjectCode: String,
+    subjectName: String,
+    subjectApplicants: number;
+}
+
+//Creates a subject block for the Lecturer
+function CreateSubject({subjectCode, subjectName, subjectApplicants}: SubjectProps)
+{
+    return(
+        <Link href={"./subjectManager"}>
+            <div className="subject-container">
+                <h2>{subjectCode}</h2>
+                <h2>{subjectName}</h2>
+                <h2>New Applicants: {subjectApplicants}</h2>
+            </div>
+        </Link>
+    );
+}
+
+export function HomeContent({isLoggedIn, accountType}: EducatorProps)
+{
+    return(
+        <div className="home-content">
+            <div className="home-grid">
+                <CreateSubject subjectCode={"COSC1121"} subjectName={"Database Applications"} subjectApplicants={30}/>
+                <CreateSubject subjectCode={"COSC1121"} subjectName={"Database Applications"} subjectApplicants={30}/>
+                <CreateSubject subjectCode={"COSC1121"} subjectName={"Database Applications"} subjectApplicants={30}/>
+                <CreateSubject subjectCode={"COSC1121"} subjectName={"Database Applications"} subjectApplicants={30}/>
+                <CreateSubject subjectCode={"COSC1121"} subjectName={"Database Applications"} subjectApplicants={30}/>
+                <CreateSubject subjectCode={"COSC1121"} subjectName={"Database Applications"} subjectApplicants={30}/>
+            </div>
+        </div>
+    )
+}
