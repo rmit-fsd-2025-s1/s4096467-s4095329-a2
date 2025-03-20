b48d37345217202b0d3acb819c2e1f72fda4b6ed
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-20 22:13:34 +1100

    Added redirect on successful login

--- teach-team-app/src/pages/login.tsx
+++ teach-team-app/src/pages/login.tsx
@@ -1,24 +1,71 @@
 import {Header} from "../components/Header/Header";
 import {Footer} from "../components/Footer/Footer";
+import {useState, FormEvent} from "react";
+import {useRouter} from "next/navigation";
+import { isPasswordValid, userState, getPasswordForUser } from "../helpers/validate";
 import Link from 'next/link'
 
 import "./sign-in.css";
 
+
+
+;
+
 export default function loginScreen()
 {
+    //Create form data using state hook
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+    
+    //Create router for redirecting on successful login
+    const router = useRouter();
+    
+    //Attempt to login on submit
+    const attemptLogin = (e: FormEvent) => 
+    {
+        e.preventDefault();
+
+        console.log(formData);
+        const email = formData.email;
+        const password = formData.password;
+        const userVal: userState = {email, password};
+        console.log(isPasswordValid(userVal));
+
+        //Redirect
+        if(isPasswordValid(userVal))
+        {
+            router.push('/educator/educator');
+        }
+        else
+        {
+            //Password is invalid
+        }
+    }
+
     return(
         <>
             <title>Login</title>
             {/* <Header isLoggedIn={true} /> */}
-            <form>
+            <form onSubmit={attemptLogin}>
                 <h1>Welcome to the TeachTeam</h1>
                 <label>
                     Email
-                    <input type="text" name="email"/>
+                    {/* On Update, set the values in formData for email */}
+                    <input type="text" name="email" 
+                        value={formData.email}
+                        onChange={(e)=>setFormData({...formData, email: e.target.value})}
+                        placeholder="Enter Email"
+                    />
                 </label>
                 <label>
                     Password
-                    <input type="password" name="password"/>
+                    {/* On Update, set the values in formData for password */}
+                    <input type="password" name="password"
+                        value={formData.password}
+                        onChange={(e)=>setFormData({...formData, password: e.target.value})}
+                    />
                 </label>
                 <div className="forgot"/*Extra stuff*/> 
                     Forgot Password? 
