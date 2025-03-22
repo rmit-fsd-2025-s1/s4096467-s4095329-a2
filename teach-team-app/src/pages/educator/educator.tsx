4fcb32c609065abd13238819c99b09be01b4aa0a
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-22 20:20:40 +1100

    educator home page now passes values using localStorage

--- teach-team-app/src/pages/educator/educator.tsx
+++ teach-team-app/src/pages/educator/educator.tsx
@@ -1,17 +1,35 @@
 import {Header} from "../../components/Header/Header";
 import {Footer} from "../../components/Footer/Footer";
 import {HomeContent} from "../../components/Home/Home";
+import { isPasswordValid, userCred, getPasswordForUser, getUserType} from "../../helpers/validate";
 
 import "../../styles/user-home.css";
+import { useEffect, useState } from "react";
 
 export default function loginScreen()
 {
+    const[localEmail, setLocalEmail] = useState<string>("");
+    const[localPassword, setLocalPassword] = useState<string>("");
+    
+    useEffect(() => 
+    {
+        setLocalEmail(localStorage.getItem("localEmail")||"");
+    }, []);
+    
+    useEffect(() => 
+    {
+        setLocalPassword(localStorage.getItem("localPassword")||"");
+    }, []);
+
+    let user: userCred = {email: localEmail, password:localPassword};
+    let passwordValid = isPasswordValid(user);
+
     return(
         <>
             <title>Lecturer Home</title>
-            <Header isLoggedIn={true} />
-            <HomeContent isLoggedIn={true} accountType="lecturer"/>
-            <Footer isLoggedIn={true} type=""/>
+            <Header isLoggedIn={passwordValid} />
+            <HomeContent isLoggedIn={passwordValid} accountType={getUserType(localEmail)||""}/>
+            <Footer isLoggedIn={passwordValid} type=""/>
         </>
     );
     
