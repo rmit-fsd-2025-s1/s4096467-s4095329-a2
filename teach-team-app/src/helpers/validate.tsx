08689d6748b6b66bbd569ecde7118b513f89e562
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-22 16:36:47 +1100

    Login now uses a Map to simulate the DB

--- teach-team-app/src/helpers/validate.tsx
+++ teach-team-app/src/helpers/validate.tsx
@@ -1,15 +1,68 @@
 export interface userState
 {
-    email: String,
-    password: String;
+    email: string,
+    password: string,
+    role: string;
 }
 
-export function isPasswordValid(user: userState)
+export interface userCred
+{
+    email: string,
+    password: string
+}
+
+export function isPasswordValid(user: userCred)
 {
     return(getPasswordForUser(user));
 }
 
-export function getPasswordForUser({email, password}: userState)
+export function getPasswordForUser({email, password}: userCred)
 {
-    return(password=="Password1"?true:false);
+    // Populates the Map, this will be done with a DB later
+    let db: Map<string, userState> = generateUsers();
+
+    //If the name is found in the Map
+    if(db.has(email))
+    {
+        //Yeah this looks like it is throwing errors, however it works somehow
+        const hashPass: string = db.get(email)?.password;
+        return(password=== hashPass?true:false);
+    }
+    else
+    {
+        return(false);
+    }
+}
+
+//Function to generate users, will use DB later
+export function generateUsers()
+{
+    const user1: userState = 
+    {
+        email: "test1@gmail.com",
+        password: "Password1",
+        role: "tutor"
+    }
+
+    const user2: userState = 
+    {
+        email: "connor@gmail.com",
+        password: "P@ssword1",
+        role: "lecture"
+    }
+
+    const user3: userState = 
+    {
+        email: "will@gmail.com",
+        password: "P@ssword2",
+        role: "lecture"
+    }
+
+    let returnMap = new Map<string, userState>();
+
+    returnMap.set("test1@gmail.com", user1);
+    returnMap.set("connor@gmail.com", user2);
+    returnMap.set("will@gmail.com", user3);
+
+    return returnMap;
 }
