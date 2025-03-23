045266444b71ce5deb66ca03b46740b7c6d97e1d
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-23 14:40:00 +1100

    Updated footer and header to now use <Link> instead of <a>

--- teach-team-app/src/components/Footer/Footer.tsx
+++ teach-team-app/src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./Footer.css";
 
 interface FooterProps
@@ -11,30 +12,31 @@ export function Footer({isLoggedIn, type}: FooterProps)
     let logOut;
     if(isLoggedIn)
     {
-        logOut = <a><p>Log Out</p></a>
+        logOut = <Link href=""><p>Log Out</p></Link>
     }
 
     return(
         <div className="footer-container">
             <span>
                 <h3>Quick Links</h3>
-                <a href="/"><p>Home</p></a>
-                <a href=""><p>About Us</p></a>
-                <a href=""><p>FAQ</p></a>
-                <a href=""><p>Privacy Policy</p></a>
-                <a href=""><p>Terms of Service</p></a>
-                <a href="/login"><p>Log In</p></a>
-                <a href="/register"><p>Register</p></a>
+                <Link href="/"><p>Home</p></Link>
+                <Link href=""><p>About Us</p></Link>
+                <Link href=""><p>FAQ</p></Link>
+                <Link href=""><p>Privacy Policy</p></Link>
+                <Link href=""><p>Terms of Service</p></Link>
+                <Link href="/login"><p>Log In</p></Link>
+                <Link href="/register"><p>Register</p></Link>
                 {logOut}
             </span>
             <span>
                 <h3>Contact Us</h3>
-                <a href=""><p>Email</p></a>
-                <a href=""><p>Phone</p></a>
+                <Link href=""><p>Email</p></Link>
+                <Link href=""><p>Phone</p></Link>
             </span>
             <span>
                 <h3>Navigation</h3>
-                <a href=""><p>Home</p></a>
+                <Link href="/"><p>Home</p></Link>
+                <Link href="/educator/educator"><p>Educator Dashboard</p></Link>
             </span>
         </div>
     );
