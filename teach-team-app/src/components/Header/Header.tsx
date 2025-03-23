98a5dc01c0fcecbd7e27085856ce318426fec1a5
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-23 14:40:00 +1100

    Updated footer and header to now use <Link> instead of <a>

--- teach-team-app/src/components/Header/Header.tsx
+++ teach-team-app/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import Link from 'next/link';
 
 interface HeaderProps
 {
@@ -14,20 +15,27 @@ export function Header({isLoggedIn}: HeaderProps)
     }
     else
     {
-        profile = <><button>Log In</button><button>Register</button></>;
+        profile = <>
+        <Link href="/login">
+            <button className="loginButton">Log In</button>
+        </Link>
+        <Link href="/register">
+        <button className="registerButton">Register</button>
+        </Link>
+        </>;
     }
 
     return(
         <div className="header-container">
-            <a href={isLoggedIn?"/educator/educator":"/"}>
-            <span className="header-section">
-                <img src="https://gumtreeau-res.cloudinary.com/image/private/t_$_75/gumtree/8adfb076-8f0f-4a22-befe-d225b5ccfca3.jpg" alt="Annoying Cat"/>
-                    <h1>TeachTeam Management</h1>
-            </span>
-                </a>
-                <span>
-                    {profile}
+            <Link href={isLoggedIn?"/educator/educator":"/"}>
+                <span className="header-section">
+                    <img src="https://gumtreeau-res.cloudinary.com/image/private/t_$_75/gumtree/8adfb076-8f0f-4a22-befe-d225b5ccfca3.jpg" alt="Annoying Cat"/>
+                        <h1>TeachTeam Management</h1>
                 </span>
+            </Link>
+            <span>
+                {profile}
+            </span>
         </div>
     )
 }
