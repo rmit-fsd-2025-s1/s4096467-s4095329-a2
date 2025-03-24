28278744715563853a5b62e0d249aa9795466d47
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-24 17:08:33 +1100

    Added chakra-ui version 3.x.x

--- teach-team-app/src/pages/_app.tsx
+++ teach-team-app/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Provider } from "@/components/ui/provider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from 'next/head';
@@ -8,7 +9,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
       </Head>
-      <Component {...pageProps} />
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
       </>
     );
 }
