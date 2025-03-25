888502721e85a97b3de6ecd06882ef13567fb43e
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-25 15:44:34 +1100

    Clicking on subjects now redirect you + validation

--- teach-team-app/src/helpers/validate.tsx
+++ teach-team-app/src/helpers/validate.tsx
@@ -4,7 +4,8 @@ export interface userState
 {
     email: string,
     password: string,
-    role: string;
+    role: string,
+    classes?: string[]
 }
 
 export interface userCred
@@ -52,6 +53,20 @@ export function getUserType(email: string)
     }
 }
 
+export function isLecturerForClass(email: string, classCode: string)
+{
+    let db: Map<string, userState> = generateUsers();
+
+    if(db.has(email))
+        {
+            if(db.get(email)?.classes?.includes(classCode))
+            {
+                return true;
+            }
+        }
+        return false;
+}
+
 //Function to generate users, will use DB later
 export function generateUsers()
 {
@@ -68,7 +83,8 @@ export function generateUsers()
         email: "connor@gmail.com",
         //P@ssword1
         password: "$2b$10$8C6C.0Ph4SljkgQchNCxuu/BaMwziIA3Uz66S/5rciwtUJflURPlK",
-        role: "lecturer"
+        role: "lecturer",
+        classes: ["COSC1121", "COSC1122", "COSC1123"]
     }
 
     const user3: userState = 
@@ -76,7 +92,8 @@ export function generateUsers()
         email: "will@gmail.com",
         //P@ssword2
         password: "$2b$10$IVaJqtZuG1oo6xj2lHi/4.20ZWCbYWPgtqA7r0.aBKrxvF699.skq",
-        role: "lecturer"
+        role: "lecturer",
+        classes: ["COSC1124", "COSC1125", "COSC1126"]
     }
 
     let returnMap = new Map<string, userState>();
