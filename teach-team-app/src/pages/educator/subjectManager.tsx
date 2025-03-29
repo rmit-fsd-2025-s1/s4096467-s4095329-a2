67e216fbaecba53f1ae812c7bb92293723f25f91
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-29 20:45:53 +1100

    Moved subject candidate logic out of table generator

--- teach-team-app/src/pages/educator/subjectManager.tsx
+++ teach-team-app/src/pages/educator/subjectManager.tsx
@@ -8,7 +8,8 @@ import { For, Stack, Table } from "@chakra-ui/react";
 import "../../styles/user-home.css";
 import { useEffect, useState } from "react";
 import { InvalidLogin } from "@/components/InvalidLogin/InvalidLogin";
-import { TutorSubjectTable } from "@/components/SortingTable/SortingTable";
+import { TutorSubjectTable, dualTableProps } from "@/components/SortingTable/SortingTable";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 
 
@@ -39,17 +40,39 @@ export default function subjectManager()
     let loginType = getUserType(user.email);
     let content;
 
-    const[candidateList, setCandidateList] = useState<userState>();
-    const[selectedList, setSelectedList] = useState<userState>();
+    //Pull sample values
+    let dbSubj: Map<string, subject> = generateSubjects();
+    let dbTut: Map<string, userState> = generateUsers();
+    //Create tutors array
+    let tutors: userState[] = [];
+
+    //SELECT <values> FROM tutors as t LEFT JOIN subject as s ON t.email = s.tutor WHERE s.subject_name = ?
+    if(dbSubj.has(subject??""))
+        {
+            dbSubj.get(subject??"")?.candidates.forEach((it) => {
+                const tutor: userState | undefined = dbTut.get(it);
+                if (tutor) {
+                    tutors.push(tutor);
+                }
+            })
+        }
+
+        //Create hooks to update the tables
+    const[candidateList, setCandidateList] = useLocalStorage<userState[]>("tempCandidateList", tutors);
+    const[selectedList, setSelectedList] = useLocalStorage<userState[]>("tempSelectedList", []);
 
     //Generate content based on logged in status
     if(passwordValid && (getUserType(localEmail) === "lecturer") && isLecturerForClass(localEmail, subject??""))
         {
+            //Generate props to be passed into the table generators.
+            let table1Props: dualTableProps = {table1: candidateList, setTable1: setCandidateList, table2: selectedList, setTable2: setSelectedList}
+            let table2Props: dualTableProps = {table2: candidateList, setTable2: setCandidateList, table1: selectedList, setTable1: setSelectedList}
+
             content = <>
                 <h2>{subject}</h2>
                 <div className="flex-sbs flex-gap">
-                    {TutorSubjectTable(subject??"")}
-                    {TutorSubjectTable(subject??"")}
+                    {TutorSubjectTable(table1Props??"")}
+                    {TutorSubjectTable(table2Props??"")}
                 </div>
             </>
             ;
