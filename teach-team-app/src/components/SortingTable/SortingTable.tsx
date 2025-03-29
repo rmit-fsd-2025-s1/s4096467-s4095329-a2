cd251a9a3a53aaf7fff6154259d48dbb7a65e280
Antipoison7 <s4096467@student.rmit.edu.au>
2025-03-29 20:45:09 +1100

    Created dualTableProps to pass into the table generation function

--- teach-team-app/src/components/SortingTable/SortingTable.tsx
+++ teach-team-app/src/components/SortingTable/SortingTable.tsx
@@ -1,24 +1,18 @@
 import { generateSubjects, generateUsers, subject, userState } from "@/helpers/validate";
 import { Table, HoverCard, Portal, Checkbox } from "@chakra-ui/react";
+import { SetStateAction } from "react";
 
-export function TutorSubjectTable(subjectName: string)
+export interface dualTableProps
 {
-    //Pull sample values
-    let dbSubj: Map<string, subject> = generateSubjects();
-    let dbTut: Map<string, userState> = generateUsers();
-    //Create tutors array
-    let tutors: userState[] = [];
+    table1: userState[],
+    setTable1: (value: SetStateAction<userState[]>) => void,
+    table2: userState[],
+    setTable2: (value: SetStateAction<userState[]>) => void;
+}
 
-    //SELECT <values> FROM tutors as t LEFT JOIN subject as s ON t.email = s.tutor WHERE s.subject_name = ?
-    if(dbSubj.has(subjectName))
-        {
-            dbSubj.get(subjectName)?.candidates.forEach((it) => {
-                const tutor: userState | undefined = dbTut.get(it);
-                if (tutor) {
-                    tutors.push(tutor);
-                }
-            })
-        }
+export function TutorSubjectTable({table1, table2, setTable1, setTable2}: dualTableProps)
+{
+
     //Create the table
     return(<Table.Root borderWidth="1px" rounded="md" interactive>
         {/* Header of the table */}
@@ -29,29 +23,29 @@ export function TutorSubjectTable(subjectName: string)
         {/* Body of the table */}
         <Table.Body>
             {/* Table Row Factory */}
-            {tutors.map((tut) => (
+            {table1.map((tut) => (
                 // On hover display tutor information
-                <HoverCard.Root openDelay={500} closeDelay={100}>
-                    <HoverCard.Trigger asChild>
-                        {/* Table Row Declaration */}
-                        <Table.Row key={tut.email}>
+                // <HoverCard.Root openDelay={500} closeDelay={100}>
+                //     <HoverCard.Trigger asChild>
+                        //Table Row Declaration
+                        <Table.Row key={tut.email} onClick={(e)=>{console.log("ae")}}>
                             {/* Display other information in body */}
                             <Table.Cell>{tut.email}</Table.Cell>
                             <Table.Cell>{tut.role}</Table.Cell>
                         </Table.Row>
-                    </HoverCard.Trigger>
-                    <Portal>
-                        <HoverCard.Positioner>
-                            <HoverCard.Content>
-                                <HoverCard.Arrow/>
-                                <div>
-                                    <h3>Email: {tut.email}</h3>
-                                    <h3>Role: {tut.role}</h3>
-                                </div>
-                            </HoverCard.Content>
-                        </HoverCard.Positioner>
-                    </Portal>
-                </HoverCard.Root>
+                //     </HoverCard.Trigger>
+                //     <Portal>
+                //         <HoverCard.Positioner>
+                //             <HoverCard.Content>
+                //                 <HoverCard.Arrow/>
+                //                 <div>
+                //                     <h3>Email: {tut.email}</h3>
+                //                     <h3>Role: {tut.role}</h3>
+                //                 </div>
+                //             </HoverCard.Content>
+                //         </HoverCard.Positioner>
+                //     </Portal>
+                // </HoverCard.Root>
                 
             ))}
         </Table.Body>
