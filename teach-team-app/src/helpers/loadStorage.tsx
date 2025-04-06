472b0e8b5920af7723c1ff6ade2dbf71d86dde13
Antipoison7 <s4096467@student.rmit.edu.au>
2025-04-06 21:34:23 +1000

    Commented and removed debug data from loadStorage

--- teach-team-app/src/helpers/loadStorage.tsx
+++ teach-team-app/src/helpers/loadStorage.tsx
@@ -1,19 +1,17 @@
-import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { userState, subject, generateSubjects, generateUsers } from "@/helpers/validate";
 
+// Interface to represent the "Database" used in localstorage
 export interface localDBInt
 {
     users: [string, userState][],
     subjects: [string, subject][]
 }
 
+// Loads the sample data into the localDBInt type and returns it to populate the localStorage
 export function loadDB(): localDBInt
 {
     const userData: [string, userState][] = Array.from(generateUsers());
     const subjectData: [string, subject][] = Array.from(generateSubjects());
-    console.log("dingus");
-    console.log(userData);
-    console.log(subjectData);
     let localStorageBD: localDBInt = {users: userData, subjects: subjectData};
     return localStorageBD;
 }
