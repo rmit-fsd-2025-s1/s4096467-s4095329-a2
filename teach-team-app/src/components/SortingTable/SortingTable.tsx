30ac37c01cf6f4e14317bef0ed107515dddd4dae
Antipoison7 <s4096467@student.rmit.edu.au>
2025-04-11 23:12:33 +1000

    Added more details to lecturer tables

--- teach-team-app/src/components/SortingTable/SortingTable.tsx
+++ teach-team-app/src/components/SortingTable/SortingTable.tsx
@@ -40,7 +40,7 @@ export function TutorSubjectTable({table1, table2, setTable1, setTable2}: dualTa
         {/* Header of the table */}
         <Table.Header>
             <Table.ColumnHeader p="4" fontSize="xl" fontWeight="bold" bg="gray.100" color="black">Tutor Name</Table.ColumnHeader>
-            <Table.ColumnHeader p="4" fontSize="xl" fontWeight="bold" bg="gray.100" color="black">Tutor Role</Table.ColumnHeader>
+            <Table.ColumnHeader p="4" fontSize="xl" fontWeight="bold" bg="gray.100" color="black">Tutor Availability</Table.ColumnHeader>
         </Table.Header>
         {/* Body of the table */}
         <Table.Body>
@@ -53,17 +53,23 @@ export function TutorSubjectTable({table1, table2, setTable1, setTable2}: dualTa
                             swapTables(index, table1, setTable1, table2, setTable2);
                             }}>
                             {/* Display other information in body */}
-                            <Table.Cell p="4" fontSize="md">{tut.email}</Table.Cell>
-                            <Table.Cell p="4" fontSize="md">{tut.role}</Table.Cell>
+                            <Table.Cell p="4" fontSize="md">{tut.name??tut.email}</Table.Cell>
+                            <Table.Cell p="4" fontSize="md">{tut.avail??"Not Provided"}</Table.Cell>
                         </Table.Row>
                     </HoverCard.Trigger>
                     <Portal>
                         <HoverCard.Positioner>
                             <HoverCard.Content>
                                 <HoverCard.Arrow/>
-                                <Box p="4">
-                                    <h3>Email: {tut.email}</h3>
-                                    <h3>Role: {tut.role}</h3>
+                                <Box p="4" className="lecturerBox">
+                                    <h2>{tut.name??"No Name Provided"}</h2>
+                                    <h3>Summary: {tut.summary??"No Summary Provided"}</h3>
+                                    <h3>Previous Roles: {tut.prevRoles??"No Previous Roles"}</h3>
+                                    <h3>Availability: {tut.avail??"No Availability Provided"}</h3>
+                                    <h3>Education: {tut.education??"No Education Provided"}</h3>
+                                    <h3>Certifications: {tut.certifications??"No Certifications Provided"}</h3>
+                                    <h3>Skills: {tut.skills??"No Skills Provided"}</h3>
+                                    <h3>Languages: {tut.languages??"No Languages Provided"}</h3>
                                 </Box>
                             </HoverCard.Content>
                         </HoverCard.Positioner>
