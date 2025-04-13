6a4ba62566bcae9fa8b83deec81e3c90e388107c
Antipoison7 <s4096467@student.rmit.edu.au>
2025-04-13 23:13:12 +1000

    Fixed typescript errors in localStorageGet

--- teach-team-app/src/helpers/localStorageGet.tsx
+++ teach-team-app/src/helpers/localStorageGet.tsx
@@ -4,8 +4,8 @@ import { generateUsers, subject, userState } from "./validate";
 // Returns tutors that have applied to the desired subject
 export function getCandidatesFrom(subject: string, localDB: localDBInt): userState[]
 {
-    let tutors: userState[] = [];
-    let dbTut: Map<string, userState> = new Map(localDB.users.map((kp) => [kp[0], kp[1]]))??generateUsers();
+    const tutors: userState[] = [];
+    const dbTut: Map<string, userState> = new Map(localDB.users.map((kp) => [kp[0], kp[1]]))??generateUsers();
 
     //https://medium.com/codingbeauty-tutorials/javascript-convert-array-to-map-12907a8a334a
     if(new Map(localDB.subjects.map((obj) => [obj[0], obj[1]])).has(subject??""))
@@ -24,8 +24,8 @@ export function getCandidatesFrom(subject: string, localDB: localDBInt): userSta
 // Returns tutors that have been accepted from the desired subject
 export function getAcceptedFrom(subject: string, localDB: localDBInt): userState[]
 {
-    let tutors: userState[] = [];
-    let dbTut: Map<string, userState> = new Map(localDB.users.map((kp) => [kp[0], kp[1]]))??generateUsers();
+    const tutors: userState[] = [];
+    const dbTut: Map<string, userState> = new Map(localDB.users.map((kp) => [kp[0], kp[1]]))??generateUsers();
 
     //https://medium.com/codingbeauty-tutorials/javascript-convert-array-to-map-12907a8a334a
     if(new Map(localDB.subjects.map((obj) => [obj[0], obj[1]])).has(subject??""))
@@ -44,9 +44,9 @@ export function getAcceptedFrom(subject: string, localDB: localDBInt): userState
 // Returns the classes a lecturer is assigned to
 export function getEducatorClasses(email: string, localClasses: [string, subject][]): subject[]
 {
-    let dbUsers: Map<string, userState> = generateUsers();
-    let formattedMap: Map<string, subject> = new Map(localClasses.map((obj) => [obj[0], obj[1]]));
-    let lecturerClasses: subject[] = [];
+    const dbUsers: Map<string, userState> = generateUsers();
+    const formattedMap: Map<string, subject> = new Map(localClasses.map((obj) => [obj[0], obj[1]]));
+    const lecturerClasses: subject[] = [];
 
     if(dbUsers.has(email))
         {
@@ -70,7 +70,7 @@ export function getLocalCandidates(email: string, localClasses: [string, subject
 
     // Then calculate candidates
     let cand = 0;
-    for (let i of classes.values()) {
+    for (const i of classes.values()) {
         cand += i.candidates.length;
     } 
 
@@ -80,7 +80,7 @@ export function getLocalCandidates(email: string, localClasses: [string, subject
 // Gets the class names that a tutor has submitted to
 export function getAppliedCourses(email: string, localClasses: subject[]): string[]
 {
-    let courses: string[] = [];
+    const courses: string[] = [];
 
     localClasses.forEach((e)=>{
         e.candidates.forEach((c)=>{
@@ -96,7 +96,7 @@ export function getAppliedCourses(email: string, localClasses: subject[]): strin
 // Gets the class names that a tutor has been accepted for
 export function getAcceptedCourses(email: string, localClasses: subject[]): string[]
 {
-    let courses: string[] = [];
+    const courses: string[] = [];
 
     localClasses.forEach((e)=>{
         e.accepted.forEach((c)=>{
@@ -145,7 +145,7 @@ export function applicationStatus(subject: subject, tutor: string): string
 
 export function getUser(user: string, db: localDBInt): userState|undefined
 {
-    let dbTut: Map<string, userState> = new Map(db.users.map((kp) => [kp[0], kp[1]]))??generateUsers();
+    const dbTut: Map<string, userState> = new Map(db.users.map((kp) => [kp[0], kp[1]]))??generateUsers();
 
     return dbTut.get(user);
 }
