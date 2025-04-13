f0859645f221238591bdf5a5f8d8899e3670f6d3
Antipoison7 <s4096467@student.rmit.edu.au>
2025-04-13 23:22:38 +1000

    Fixed typescript errors with chakra-ui

--- teach-team-app/src/components/ui/provider.tsx
+++ teach-team-app/src/components/ui/provider.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import type { ThemeProviderProps } from "next-themes";
 import {
   ColorModeProvider,
-  type ColorModeProviderProps,
 } from "./color-mode"
 
-export function Provider(props: ColorModeProviderProps) {
+export function Provider(props: ThemeProviderProps) {
   return (
     <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
