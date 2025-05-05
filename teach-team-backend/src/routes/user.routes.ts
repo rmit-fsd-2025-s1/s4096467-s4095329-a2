3e2974993f9da480d356153ace336e29488c3457
Antipoison7 <s4096467@student.rmit.edu.au>
2025-05-05 13:01:10 +1000

    Feature: Added /login path to API

--- teach-team-backend/src/routes/user.routes.ts
+++ teach-team-backend/src/routes/user.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { UserController } from "../controller/UserController";
+import { LoginController } from "../controller/LoginController";
 
 const router = Router();
 const userController = new UserController();
+const loginController = new LoginController();
 
 router.get("/users", async (req, res) => {
   await userController.all(req, res);
+});
+
+router.get("/login/:email/:password", async (req, res) => {
+  await loginController.login(req, res);
 });
 export default router;
