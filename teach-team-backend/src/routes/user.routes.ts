a64c63fc75915c1f596fec58526269dccf3e8121
Antipoison7 <s4096467@student.rmit.edu.au>
2025-05-05 13:02:09 +1000

    Refactor: Documented usage of API router

--- teach-team-backend/src/routes/user.routes.ts
+++ teach-team-backend/src/routes/user.routes.ts
@@ -6,10 +6,12 @@ const router = Router();
 const userController = new UserController();
 const loginController = new LoginController();
 
+// to call this, run /api/users/
 router.get("/users", async (req, res) => {
   await userController.all(req, res);
 });
 
+// to call this, run /api/login/yourEmailHere/yourPasswordHere
 router.get("/login/:email/:password", async (req, res) => {
   await loginController.login(req, res);
 });
