c71d791eacdac56a6cb75466dc25f797867e9228
Antipoison7 <s4096467@student.rmit.edu.au>
2025-05-05 13:00:52 +1000

    Feature: Created basic login checking (Not secure)

---
+++ teach-team-backend/src/controller/LoginController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Users } from "../entity/Users";
+import bcrypt from "bcryptjs";
+
+export class LoginController {
+
+  /**
+   * Retrieves all users from the database
+   * @param request - Express request object
+   * @param response - Express response object
+   * @returns JSON response containing an array of all users
+   */
+  async login(request: Request, response: Response) {
+    const inEmail: string = request.params.email;
+    const inPassword: string = request.params.password;
+    const user: Users[] = await AppDataSource.manager.find(Users, {
+      where: {
+        email: inEmail
+        }
+      });
+
+    if(user.length > 0)
+      {
+        return(response.json(bcrypt.compareSync(inPassword, user[0].password)));
+      }
+    else
+    {
+      return(response.json(false));
+    }
+  }
+}
