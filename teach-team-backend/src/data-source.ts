29fd787bcbe725d17dca29a2edd322dc75326142
William Lor <s4095329@student.rmit.edu.au>
2025-05-06 15:43:20 +1000

    update typeorm

--- teach-team-backend/src/data-source.ts
+++ teach-team-backend/src/data-source.ts
@@ -8,12 +8,12 @@ import { Languages } from "./entity/Languages"
 import { Skills } from "./entity/Skills"
 
 export const AppDataSource = new DataSource({
-    type: "",
-    host: "",
-    port: ,
-    username: "",
-    password: "",
-    database: "",
+    type: "mysql",
+    host: "209.38.26.237",
+    port: 3306,
+    username: "S4095329",
+    password: "S4095329",
+    database: "S4095329",
     synchronize: false,
     logging: false,
     entities: [Users, Certifications, Educations, Languages, Previous_Roles, Skills],
