88109efa1d6066e0e380467243ab9bf75971e9f2
Antipoison7 <s4096467@student.rmit.edu.au>
2025-05-11 21:31:24 +1000

    Feature: Added Populate_Classes sql file to generate template classes

---
+++ queries/Populate_Classes.sql
@@ -0,0 +1,21 @@
+INSERT INTO classes (class_code, subject_name) VALUES
+('ISYS3413', 'Software Engineering Fundamentals for IT'),
+('COSC2758', 'Full Stack Development'),
+('COSC2801', 'Programming Bootcamp 1'),
+('COSC2757', 'Cloud Foundations'),
+('ISYS1102', 'Database Applications'),
+('COSC3046', 'Web Programming Studio');
+
+INSERT INTO lecturer_classes (class_code, email) VALUES
+('COSC2757', 'connor@gmail.com'),
+('COSC2758', 'connor@gmail.com'),
+('COSC2801', 'connor@gmail.com'),
+('COSC3046', 'will@gmail.com'),
+('ISYS1102', 'will@gmail.com'),
+('ISYS3413', 'will@gmail.com');
+
+
+SELECT subject_name, classes.class_code, full_name
+FROM classes
+LEFT JOIN lecturer_classes ON classes.class_code = lecturer_classes.class_code
+LEFT JOIN users ON lecturer_classes.email = users.email;
