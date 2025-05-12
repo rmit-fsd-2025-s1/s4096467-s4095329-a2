0d6ad8aa328ffb7ea593e21072a4a16b1a66b830
Antipoison7 <s4096467@student.rmit.edu.au>
2025-05-12 16:45:57 +1000

    Feature: Created Tutors link table

---
+++ teach-team-backend/src/entity/Tutors.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, ManyToOne, PrimaryColumn, JoinColumn, OneToMany } from "typeorm"
+import { Users } from "./Users"
+import { Classes } from "./Classes"
+import { Comments } from "./Comments";
+
+@Entity()
+export class Tutors {
+    @PrimaryColumn("uuid")
+    email: string;
+    
+    @PrimaryColumn("uuid")
+    class_code: string;
+
+    @Column({type: "varchar", length: 255})
+    role_name: string
+
+    @Column({type: "bit"})
+    accepted: boolean
+
+    @Column({type: "bit"})
+    active_tutor: boolean
+
+    @OneToMany(() => Comments, (comment) => comment.tutor, {eager: true})
+    comments: Comments[]
+
+    @ManyToOne(() => Users, (user) => user.email, {eager: true})
+    @JoinColumn({ name: "email" })
+    user: Users
+    
+    @ManyToOne(() => Classes, (cls) => cls.class_code)
+    @JoinColumn({ name: "class_code" })
+    classes: Classes
+}
