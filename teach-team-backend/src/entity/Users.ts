49eb93bed3eb7817ac2e6a0b27af07e3bc5ccb8b
Antipoison7 <s4096467@student.rmit.edu.au>
2025-05-12 16:47:57 +1000

    Feature: Added Active boolean parameter to Users - TypeORM

--- teach-team-backend/src/entity/Users.ts
+++ teach-team-backend/src/entity/Users.ts
@@ -1,49 +1,56 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToMany } from "typeorm"
+import { Entity, Column, OneToMany, ManyToMany } from "typeorm"
 import { Certifications } from "./Certifications"
 import { Educations } from "./Educations"
 import { Languages } from "./Languages"
 import { Previous_Roles } from "./Previous_Roles"
 import { Skills } from "./Skills"
 import { Classes } from "./Classes"
+import { Tutors } from "./Tutors"
 
 @Entity()
 export class Users {
 
-    @Column({ primary: true })
+    @Column({ primary: true, type: "uuid" })
     email: string
 
-    @Column()
+    @Column({type: "varchar", length: 1000})
     full_name: string
 
-    @Column()
+    @Column({type: "char", length: 60})
     password: string
 
-    @Column()
+    @Column({type: "varchar", length: 9})
     role: string
 
-    @Column()
+    @Column({type: "varchar", length: 9})
     availability: string
     
-    @Column()
+    @Column({type: "text"})
     summary: string
 
-    @OneToMany(() => Certifications, (certification) => certification.user_key, {eager:true,})
+    @Column({type: "boolean", default: true})
+    active: boolean
+
+    @OneToMany(() => Certifications, (certification) => certification.user_key, {cascade: true, eager:true,})
     certifications: Certifications[]
 
-    @OneToMany(() => Educations, (education) => education.user_key, {eager:true,})
+    @OneToMany(() => Educations, (education) => education.user_key, {cascade: true, eager:true,})
     educations: Educations[]
 
-    @OneToMany(() => Languages, (language) => language.user_key, {eager:true,})
+    @OneToMany(() => Languages, (language) => language.user_key, {cascade: true, eager:true,})
     languages: Languages[]
 
-    @OneToMany(() => Previous_Roles, (role) => role.user_key, {eager:true,})
+    @OneToMany(() => Previous_Roles, (role) => role.user_key, {cascade: true, eager:true,})
     previous_roles: Previous_Roles[]
 
-    @OneToMany(() => Skills, (skill) => skill.user_key, {eager:true,})
+    @OneToMany(() => Skills, (skill) => skill.user_key, {cascade: true, eager:true,})
     skills: Skills[]
 
     @ManyToMany(() => Classes, (classes) => classes.lecturers)
     lectures: Classes[]
+
+    @OneToMany(() => Tutors, (tutor) => tutor.user)
+    tutorJoin: Tutors[]
 }
 
 export { Certifications }
