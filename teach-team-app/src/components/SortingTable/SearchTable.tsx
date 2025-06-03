4a9dbd21f3ff00445f2da4998a08299e3a158c16
Antipoison7 <s4096467@student.rmit.edu.au>
2025-06-03 15:18:45 +1000

    Feature: Search table pulls data | TODO: Add searching

--- teach-team-app/src/components/SortingTable/SearchTable.tsx
+++ teach-team-app/src/components/SortingTable/SearchTable.tsx
@@ -1,8 +1,24 @@
-import { getAcceptedCount, getAcceptedCourses, getAppliedCourses } from "@/helpers/localStorageGet";
-import { subject, userState } from "@/helpers/validate";
+import { subject, User, userState } from "@/helpers/validate";
 import { Box, HoverCard, Portal, Table } from "@chakra-ui/react";
 
-type searchTableProps = {tableArr: userState[], classes: subject[], type: string, keyword: string, order: string, sort?: string}
+type searchTableProps = {tableDataIn: userData[]}
+
+export interface userData{
+    person: User,
+    applied: {
+        courseCode: string,
+        courseName: string,
+        tutorStatus: boolean,
+        labStatus: boolean
+    }[],
+    accepted: {
+        courseCode: string,
+        courseName: string,
+        tutorStatus: boolean,
+        labStatus: boolean
+    }[],
+    timesAccepted: number
+}
 
 // Searches for key word based on inputted type
 function keywordFilter(keyWord: string, type: string, table: userState[], subject: subject[]) : userState[]
@@ -84,41 +100,41 @@ function keywordFilter(keyWord: string, type: string, table: userState[], subjec
     return tempTable;
 }
 
-export function SearchTable({ tableArr, classes, type, keyword, sort}: searchTableProps)
+export function SearchTable({ tableDataIn }: searchTableProps)
 {
-    let formatTable = [...tableArr];
+    // let formatTable = [...tableArr];
 
-    console.log(sort);
+    // console.log(sort);
 
     //https://owlcation.com/stem/creating-a-sortable-list-in-react-js  
-    if (sort !== "none") {
-        formatTable.sort((a, b) => {
-            //Compares two elements. so if one were greater then the other than the other (by subtraction) then put that element after
-            const A = getAcceptedCount(a.email, classes) || 0;
-            const B = getAcceptedCount(b.email, classes) || 0;
+    // if (sort !== "none") {
+    //     formatTable.sort((a, b) => {
+    //         //Compares two elements. so if one were greater then the other than the other (by subtraction) then put that element after
+    //         const A = getAcceptedCount(a.email, classes) || 0;
+    //         const B = getAcceptedCount(b.email, classes) || 0;
             
-            //least
-            if (sort === "ascending") {
-                return A - B;
-            } 
-            //most
-            else {
-                return B - A;
-            };
-        });
-    }
+    //         //least
+    //         if (sort === "ascending") {
+    //             return A - B;
+    //         } 
+    //         //most
+    //         else {
+    //             return B - A;
+    //         };
+    //     });
+    // }
     
-    if(sort === "none Accepted")
-        {
-            formatTable = formatTable.filter((e)=>{
-                const acceptedCount = getAcceptedCount(e.email, classes) || 0
-                return acceptedCount === 0;
-        })}
+    // if(sort === "none Accepted")
+    //     {
+    //         formatTable = formatTable.filter((e)=>{
+    //             const acceptedCount = getAcceptedCount(e.email, classes) || 0
+    //             return acceptedCount === 0;
+    //     })}
 
-    if(keyword.length > 0) // If something has been written into the search box
-        {
-            formatTable = keywordFilter(keyword, type, formatTable, classes);
-        }
+    // if(keyword.length > 0) // If something has been written into the search box
+    //     {
+    //         formatTable = keywordFilter(keyword, type, formatTable, classes);
+    //     }
 
     return(<Table.Root 
             variant="outline" 
@@ -142,20 +158,19 @@ export function SearchTable({ tableArr, classes, type, keyword, sort}: searchTab
         {/* Body of the table */}
         <Table.Body>
             {/* Table Row Factory */}
-            {formatTable.map((tut) => (
-                tut.role == "tutor" ?(
+            {tableDataIn.map((tut) => (
                 // On hover display tutor information
-                <HoverCard.Root key={tut.email} openDelay={500} closeDelay={100}>
+                <HoverCard.Root key={tut.person.email} openDelay={500} closeDelay={100}>
                     <HoverCard.Trigger asChild>
-                        <Table.Row key={tut.email}>
+                        <Table.Row key={tut.person.email}>
                             {/* Display other information in body */}
-                            <Table.Cell p="4" fontSize="md">{tut.name??"Not Provided"}</Table.Cell> {/*Tutor Name*/}
-                            <Table.Cell p="4" fontSize="md">{tut.email??"Not Provided"}</Table.Cell> {/*Tutor Email*/}
-                            <Table.Cell p="4" fontSize="md">{getAppliedCourses(tut.email, classes).length >= 1 ? getAppliedCourses(tut.email, classes).map((x, i) => <tr key={i}>{x}</tr>) : "Not Provided"}</Table.Cell> {/*Applied Courses*/}
-                            <Table.Cell p="4" fontSize="md">{getAcceptedCourses(tut.email, classes).length >= 1 ? getAcceptedCourses(tut.email, classes).map((x, i) => <tr key={i}>{x}</tr>) : "Not Provided"}</Table.Cell> {/*Accepted Courses*/}
-                            <Table.Cell p="4" fontSize="md">{tut.avail && tut.avail[0].length >= 1 ? tut.avail : ["Not Provided"].map((x)=><tr key={x}>{x}</tr>)}</Table.Cell> {/*Availability*/}
-                            <Table.Cell p="4" fontSize="md">{tut.skills && tut.skills[0].length >= 1 ? tut.skills : ["Not Provided"].map((x)=><tr key={x}>{x}</tr>)}</Table.Cell> {/*Skills*/}
-                            <Table.Cell p="4" fontSize="md">{getAcceptedCount(tut.email, classes)??"# Not Calculated"}</Table.Cell> {/*Accepted*/}
+                            <Table.Cell p="4" fontSize="md">{tut.person.full_name??"Not Provided"}</Table.Cell> {/*Tutor Name*/}
+                            <Table.Cell p="4" fontSize="md">{tut.person.email??"Not Provided"}</Table.Cell> {/*Tutor Email*/}
+                            <Table.Cell p="4" fontSize="md">{tut.applied.length >= 1 ? tut.applied.map((x, i) => <tr key={i}>{ x.courseName + " - " + x.courseCode}</tr>) : "Not Provided"}</Table.Cell> {/*Applied Courses*/}
+                            <Table.Cell p="4" fontSize="md">{tut.accepted.length >= 1 ? tut.accepted.map((x, i) => <tr key={i}>{x.courseName + " - " + x.courseCode}</tr>) : "Not Provided"}</Table.Cell> {/*Accepted Courses*/}
+                            <Table.Cell p="4" fontSize="md">{tut.person.availability.length >= 1 ? tut.person.availability : ["Not Provided"].map((x)=><tr key={x}>{x}</tr>)}</Table.Cell> {/*Availability*/}
+                            <Table.Cell p="4" fontSize="md">{Array.isArray(tut.person.skills) && tut.person.skills.length >= 1 ? tut.person.skills.map((x, i) => <tr key={i}>{x}</tr>) : "Not Provided"}</Table.Cell> {/*Skills*/}
+                            <Table.Cell p="4" fontSize="md">{tut.timesAccepted??"# Not Calculated"}</Table.Cell> {/*Accepted*/}
                         </Table.Row>
                     </HoverCard.Trigger>
                     <Portal>
@@ -163,20 +178,19 @@ export function SearchTable({ tableArr, classes, type, keyword, sort}: searchTab
                             <HoverCard.Content>
                                 <HoverCard.Arrow/>
                                 <Box p="4" className="lecturerBox">
-                                    <h2>{tut.name??"No Name Provided"}</h2>
-                                    <h3>Summary: {tut.summary??"No Summary Provided"}</h3>
-                                    <h3>Previous Roles: {tut.prevRoles??"No Previous Roles"}</h3>
-                                    <h3>Availability: {tut.avail??"No Availability Provided"}</h3>
-                                    <h3>Education: {tut.education??"No Education Provided"}</h3>
-                                    <h3>Certifications: {tut.certifications??"No Certifications Provided"}</h3>
-                                    <h3>Skills: {tut.skills??"No Skills Provided"}</h3>
-                                    <h3>Languages: {tut.languages??"No Languages Provided"}</h3>
+                                    <h2>{tut.person.full_name??"No Name Provided"}</h2>
+                                    <h3>Summary: {tut.person.summary??"No Summary Provided"}</h3>
+                                    <h3>Previous Roles: {tut.person.previous_roles??"No Previous Roles"}</h3>
+                                    <h3>Availability: {tut.person.availability??"No Availability Provided"}</h3>
+                                    <h3>Education: {tut.person.educations??"No Education Provided"}</h3>
+                                    <h3>Certifications: {tut.person.certifications??"No Certifications Provided"}</h3>
+                                    <h3>Skills: {tut.person.skills??"No Skills Provided"}</h3>
+                                    <h3>Languages: {tut.person.languages??"No Languages Provided"}</h3>
                                 </Box>
                             </HoverCard.Content>
                         </HoverCard.Positioner>
                     </Portal>
                 </HoverCard.Root>
-                ):<></>
             ))}
         </Table.Body>
     </Table.Root>
