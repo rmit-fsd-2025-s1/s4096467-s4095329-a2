fb50c483b3d17ef36ce489010160b35cd1b78ace
Antipoison7 <s4096467@student.rmit.edu.au>
2025-06-03 18:05:50 +1000

    Refactor: Removed unused client side searching from SearchTable

--- teach-team-app/src/components/SortingTable/SearchTable.tsx
+++ teach-team-app/src/components/SortingTable/SearchTable.tsx
@@ -1,5 +1,5 @@
 import { toSentenceCase } from "@/helpers/stringHelper";
-import { subject, User, userState } from "@/helpers/validate";
+import { User } from "@/helpers/validate";
 import { Box, HoverCard, Portal, Table } from "@chakra-ui/react";
 
 type searchTableProps = {tableDataIn: userData[]}
@@ -21,122 +21,8 @@ export interface userData{
     timesAccepted: number
 }
 
-// Searches for key word based on inputted type
-function keywordFilter(keyWord: string, type: string, table: userState[], subject: subject[]) : userState[]
-{
-    let tempTable = [...table];
-
-    const formattedKeyword = keyWord.toLowerCase();
-
-    switch(type)
-    {
-            //Check by courses
-        case "Course":
-            //Getting the list of tutors returned from the lookup
-            let tutKey: string[] = [];
-
-            let tempKey11: string[] = [];
-            let tempKey12: string[] = [];
-            let tempKey21: string[] = [];
-            let tempKey22: string[] = [];
-
-            tempKey11 = subject.filter((e)=>{
-                return e.code.toLowerCase().includes(formattedKeyword) ?? false
-            }).flatMap((val) => val.accepted);
-
-            tempKey12 = subject.filter((e)=>{
-                return e.code.toLowerCase().includes(formattedKeyword) ?? false
-            }).flatMap((val) => val.candidates);
-
-            tempKey21 = subject.filter((e)=>{
-                return e.subjectName.toLowerCase().includes(formattedKeyword) ?? false
-            }).flatMap((val) => val.accepted);
-
-            tempKey22 = subject.filter((e)=>{
-                return e.subjectName.toLowerCase().includes(formattedKeyword) ?? false
-            }).flatMap((val) => val.candidates);
-
-            // https://stackoverflow.com/questions/3629817/getting-a-union-of-two-arrays-in-javascript
-
-            const u1: string[] = [...new Set([...tempKey11, ...tempKey12])]; 
-            const u2: string[] = [...new Set([...tempKey21, ...tempKey22])]; 
-
-            tutKey = [...new Set([...u1, ...u2])];
-
-            //Applying the found values to tempTable
-            tempTable = tempTable.filter((e)=>{
-                return tutKey.map((email) => email.toLowerCase()).includes(e.email.toLowerCase()) ?? false;
-            });
-
-            break;
-            //Check by name
-        case "Name":
-            tempTable = tempTable.filter((e)=>{
-                return (e.name && e.name.length >=1 ? e.name : "Not Provided")?.toLowerCase().includes(formattedKeyword) ?? false
-            });
-            break;
-            //Check by Skills
-        case "Skill":
-            tempTable = tempTable.filter((e)=>{
-                //This ensures its an array otherwise it will cause issues
-                return Array.isArray(e.skills) ? e.skills.some((skill) => (skill && skill.length > 0 ? skill : "Not Provided").toLowerCase().includes(formattedKeyword) ) : false;
-            });
-            break;
-            //Check by availability
-        case "Availability":
-            tempTable = tempTable.filter((e)=>{
-                return Array.isArray(e.avail) ? e.avail.some((avail) => (avail && avail.length > 0 ? avail : "Not Provided").toLowerCase().includes(formattedKeyword) ) : false;
-            });
-            break;
-        default:
-            alert("Bruh, what happened");
-        // if (sort === "none") {
-        //     tempTable = tempTable.filter((e)=>{
-        //         const acceptedCount = getAcceptedCount(e.email, classes);
-        //         return !acceptedCount || acceptedCount === 0;
-        //     });
-        // }
-    }
-
-    return tempTable;
-}
-
 export function SearchTable({ tableDataIn }: searchTableProps)
 {
-    // let formatTable = [...tableArr];
-
-    // console.log(sort);
-
-    //https://owlcation.com/stem/creating-a-sortable-list-in-react-js  
-    // if (sort !== "none") {
-    //     formatTable.sort((a, b) => {
-    //         //Compares two elements. so if one were greater then the other than the other (by subtraction) then put that element after
-    //         const A = getAcceptedCount(a.email, classes) || 0;
-    //         const B = getAcceptedCount(b.email, classes) || 0;
-            
-    //         //least
-    //         if (sort === "ascending") {
-    //             return A - B;
-    //         } 
-    //         //most
-    //         else {
-    //             return B - A;
-    //         };
-    //     });
-    // }
-    
-    // if(sort === "none Accepted")
-    //     {
-    //         formatTable = formatTable.filter((e)=>{
-    //             const acceptedCount = getAcceptedCount(e.email, classes) || 0
-    //             return acceptedCount === 0;
-    //     })}
-
-    // if(keyword.length > 0) // If something has been written into the search box
-    //     {
-    //         formatTable = keywordFilter(keyword, type, formatTable, classes);
-    //     }
-
     return(<Table.Root 
             variant="outline" 
             size="lg" 
