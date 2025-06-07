37ff39b7e145f24aba3a5265aa546be819b74547
Antipoison7 <s4096467@student.rmit.edu.au>
2025-06-07 23:05:33 +1000

    Feature: Added loading and login components to admin pages

---
+++ teach-team-admin/src/components/Footer/LoadingScreen/LoadingScreen.tsx
@@ -0,0 +1,28 @@
+//A function that generates a box that tells you to log in
+import { InvalidLogin } from "@/components/InvalidLogin/InvalidLogin";
+import { Spinner } from "@chakra-ui/react"
+import styles from "./LoadingScreen.module.css"
+import { useEffect, useState} from "react";
+
+export function LoadingScreen()
+{
+    const [message, setMessage] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setMessage(true), 5000); 
+        console.log("5 seconds have passed")
+    }, []);
+
+    return(
+        <div className={styles["load"]}>
+            {message ? (
+                <InvalidLogin />
+            ) : (
+                <>
+                    <h1>Loading...</h1>
+                    <Spinner color="purple" size="xl" borderWidth="4px" />
+                </>
+            )}
+        </div>
+    );
+}
---
+++ teach-team-admin/src/components/InvalidLogin/InvalidLogin.tsx
@@ -0,0 +1,22 @@
+//A function that generates a box that tells you to log in
+import styles from "./InvalidLogin.module.css"
+import {Button} from "@chakra-ui/react";
+import {useRouter} from "next/navigation";
+
+export function InvalidLogin()
+{
+    const router = useRouter();
+    const logIn = () => {
+        router.push('/');
+    }; 
+
+    return(
+        <>
+        <div className={styles["page"]}>           
+            <h4>If you are not logged in, you do not have permission to view this page.</h4>
+            <h4>Please log in to your account to gain access.</h4>
+            <Button variant='subtle' size='2xl' width="3" style={{padding: '0px 40px'}} onClick={logIn}>Log in</Button>
+        </div>
+        </>
+    );
+}
